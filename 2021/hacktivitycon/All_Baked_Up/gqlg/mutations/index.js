f81bc6464bab44fdbbab7c91ce8c0eeeaafa2622
Stanley <stanleyhalim.work@gmail.com>
2022-06-19 21:25:19 +0700

    2021

--- 2021/hacktivitycon/All_Baked_Up/gqlg/mutations/index.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports.authenticateUser = fs.readFileSync(path.join(__dirname, 'authenticateUser.gql'), 'utf8');
-module.exports.createPost = fs.readFileSync(path.join(__dirname, 'createPost.gql'), 'utf8');
-module.exports.createUser = fs.readFileSync(path.join(__dirname, 'createUser.gql'), 'utf8');
